513c18b612a0da188519bd0185c6e7f69cea0e73
Skiperpol <skiperpol1@gmail.com>
2022-10-18 00:45:41 +0200

    send

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Kalkulator from './pages/Kalkulator';
 function App() {
   return (
     <div>
-    1234523523
       <Routes>
         <Route exact path='/' element={<Home/>} />
         <Route path='MathCompetition/kalkulator' element={<Kalkulator/>} />
